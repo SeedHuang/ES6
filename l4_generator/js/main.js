b779edcb99798357804ffc4afbdd7e3ddaafef0c
huangchunhua <huangchunhua@baidu.com>
2017-08-31 14:09:30 +0800

    add yield

--- l4_generator/js/main.js
+++ l4_generator/js/main.js
@@ -1,33 +1,82 @@
-function * Generator () {
-    yield 1;
-    yield 2;
-    yield 3;
-    return "ending"
+// function * Generator () {
+//     yield 1;
+//     yield 2;
+//     yield 3;
+//     return "ending"
+// }
+//
+// var generator = Generator();
+// console.log([...generator]);
+// console.log(generator.next());
+// console.log(generator.next());
+// console.log(generator.next());
+// console.log(generator.next());
+//
+//
+// function * Generator2 () {
+//     yield "aa";
+//     yield "bb";
+//     yield "cc";
+//     return "ending"
+// }
+//
+// var generator2 = Generator2();
+// console.log(generator2);
+// var a = {
+//     0:1,
+//     1:2,
+//     2:3,
+//     length: 3,
+//     [Symbol.iterator]:function(){return generator2}
+// }
+//
+// console.log([...a]);
+
+
+// let a = [1, 2, 3, 4, 5, 6, 7];
+//
+// function* Generator() {
+//     for(let i=0; i < a.length; i++ ) {
+//         yield a[i];
+//     }
+//     return "ending";
+// }
+//
+//
+// let generator = Generator();
+//
+//
+// console.log(generator.next());
+// console.log(generator.next());
+// console.log(generator.next());
+// console.log(generator.next());
+// console.log(generator.next());
+// console.log(generator.next());
+// console.log(generator.next());
+// console.log(generator.next());
+
+
+function foo(a, b){
+    debugger;
+    console.log(a, b);
 }
 
-var generator = Generator();
-console.log([...generator]);
-console.log(generator.next());
-console.log(generator.next());
-console.log(generator.next());
-console.log(generator.next());
-
-
-function * Generator2 () {
-    yield "aa";
-    yield "bb";
-    yield "cc";
-    return "ending"
+function* testyield(){
+    let a = yield 1;
+    console.log(a);
+    let b = yield 2
+    console.log(b);
+    let result = a + b;
+    console.log(result);
 }
 
-var generator2 = Generator2();
-console.log(generator2);
-var a = {
-    0:1,
-    1:2,
-    2:3,
-    length: 3,
-    [Symbol.iterator]:function(){return generator2}
-}
+var testgenerat = testyield();
+var lastValue = testgenerat.next().value;
 
-console.log([...a]);
+console.log(lastValue);
+
+lastValue = testgenerat.next(lastValue).value;
+
+console.log(lastValue);
+
+lastValue = testgenerat.next(lastValue).value;
--- l4_generator/readme.md
+++ l4_generator/readme.md
@@ -138,3 +138,54 @@ var iterableObj = {
 
 console.log([...iterableObj]);
 ```
+
+## yield
+`yield`和`Generator`密不可分，因为`yield`只有在`Generator`之内才可以使用。
+
+执行方式：`yield`提供了函数执行的暂停方式，在函数运行的时候，当遇到`yield`之后就暂停之后的操作，并且将紧跟在`yield`的后面的表达式的值，作为`next`方法返回对象`value`的属性的值。`yield是`暂停的标志，next是运行接下来逻辑的按钮。`yield`的返回值为空，但是可以通过给next方设置参数来返回上一次yield运行后的值。
+
+> ⚠️ next的第一个参数，就是上一次`yield`继续执行时的值，如果不设置这个参数，`yield`继续执行当时的值为`undefined`;
+
+```
+function next (yieldReturnValue){
+    ...
+}
+```
+
+***参数***
+
+name    | type          |descriptioin
+--------|---------------|---------------------------
+name    |object/any     |上一次`yield`继续执行时的值，如果不设置这个参数，`yield`继续执行当时的值为`undefined`
+
+***Example***
+
+```
+
+function* TestYield(){
+    let a = yield 1;
+    console.log("test run value a:", a);
+    let b = yield 2;
+    console.log("test run value b:", b);
+    let result = a + b;
+    console.log("test run value result:", a);
+}
+/***this is execute***/
+let generator = TestYield();
+
+let lastValue = generator.next().value;
+console.log("get last value:", lastValue);
+// get last value: 1
+
+lastValue = generator.next(lastValue).value;
+// test run value a: 1
+console.log("get last value:", lastValue);
+get last value: 2
+
+lastValue = generator.next(lastValue).value;
+// test run value b: 2
+// test run value result: 1
+
+```
+
+![yieldruningsteps](./imgs/yieldruningsteps.png)
